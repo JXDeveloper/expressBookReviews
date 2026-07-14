6a40ab2ba89b286042c22d087aaf2fad8ac559f3
shurahbil <shurahbil.abd@gmail.com>
2026-07-14 15:35:08 +0500

    Task 5 done with all tests and ai submission file

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -68,8 +68,19 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (isbn) {
+    const book = books[isbn];
+
+    if (book) {
+      return res.status(200).send(JSON.stringify({ reviews: book.reviews }));
+    }
+    return res.status(200).json({ message: "Book was not found with this ISBN" });
+  }
+  return res.status(400).json({
+    error: "bad request",
+    message: "Missing required field: 'isbn'",
+  });
 });
 
 module.exports.general = public_users;
