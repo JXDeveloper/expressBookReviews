4bcef0135b17ea88e7c9b275693a0824efef6e29
shurahbil <shurahbil.abd@gmail.com>
2026-07-14 16:46:11 +0500

    Task 7 done with all tests and ai submission file
    login endpoint completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,53 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) =>
+  !!users.find((user) => user.username === username && user.password === password);
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username) {
+    return res.status(400).json({
+      error: "bad request",
+      message: "Missing required field: 'username'",
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({
+      error: "bad request",
+      message: "Missing required field: 'password'",
+    });
+  }
+
+  if (authenticatedUser(username, password)) {
+    const accessToken = jwt.sign({ data: password }, "my_secret_key", { expiresIn: 60 * 60 });
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send("User successfully logged in");
+  }
+
+  return res.status(208).json({ message: "Invalid Login. Check username and password" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
